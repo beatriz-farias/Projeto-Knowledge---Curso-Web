d1df1ee1864c440e2a3c27db22e58b030adfb9ab
Beatriz Farias <beatrizf@ejcm.com.br>
2023-02-22 20:03:52 -0300

    update: Removing unnecessary comments in passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,29 +1,3 @@
-// const { authSecret } = require('../.env');
-// const passport = require('passport');
-// const passportJwt = require('passport-jwt');
-// const { Strategy, ExtractJwt } = passportJwt;
-
-// module.export = app => {
-//     const params = {
-//         secretOrKey: authSecret,
-//         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-//     }
-
-//     const strategy = new Strategy(params, (payload, done) => {
-//         app.db('users')
-//             .where({ id: payload.id })
-//             .first()
-//             .then(user => done(null, user ? { ...payload } : false))
-//             .catch(err => done(err, false))
-//     })
-
-//     passport.use(strategy);
-
-//     return {
-//         authenticate: () => passport.authenticate('jwt', { session: false })
-//     }
-// }
-
 const { authSecret } = require('../.env')
 const passport = require('passport')
 const passportJwt = require('passport-jwt')
